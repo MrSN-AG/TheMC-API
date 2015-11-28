41b05ace8c11ae5fe77377e207b53827c7802603
MrSN-AG <mail@gaiduchik.com>
2015-11-28 23:14:49 +0300

    First UPLOAD
    AuthOP - ver 0.1

---
+++ src/AuthOP/Main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AuthOP;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function onJoin(PlayerJoinEvent $e){
+$p = $e->getPlayer();
+if($p->getName == "MrSN";
+$a = $p->getAddress == "IP ADRESS"){
+$p->sendMessage("TheMC: Succsessful");
+}else{
+$p->kickPlayer();
+}
+}
+}
