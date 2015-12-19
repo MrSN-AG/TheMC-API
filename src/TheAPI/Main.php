5c771ae042e86415954c91d2ee23efe095bf9a2d
MrSN-AG <mail@gaiduchik.com>
2015-12-19 23:12:21 +0300

    Update plugin
    Version - 0.0.3

--- src/TheAPI/Main.php
+++ src/TheAPI/Main.php
@@ -9,40 +9,93 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 use pocketmine\entity\Entity;
+use pocketmine\event\server\QueryRegenerateEvent;
 
 class Main extends PluginBase implements Listener{
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ЧТО НАДА - 1. Увеличить кол-во блоков в инвентаре 2. Ломаем блоки, получаем бонус 3. Запрет выкидывать предметы от вип и далее
 
 
-// РЕГИСТЕРИМ ИВЕНТЫ С САМОГО ЗАПУСКА
+// РЕГИСТЕРИМ ИВЕНТЫ С САМОГО ЗАПУСКА/////////////////////////////////////////
 public function onEnable(){
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//СЕРВЕР ИНВЕНТАРЬ
-public function onJoin(PlayerJoinEvent $inv){ 
+#########################################################################################################################
+//Инвентарь игроков
+public function onDonateJoinINV(PlayerJoinEvent $inv){ 
 	$p = $inv->getPlayer(); 
 		$p->getName(); 
 			if($inv->getPlayer()->hasPermission("inv.vip")){ 
-					$p->getInventory()->setSize(10); 
-									}
+					$p->getInventory()->setSize(50);}
+			elseif ($inv->getPlayer()->hasPermission("inv.creative")){ 
+					$p->getInventory()->setSize(70);} 
+			elseif ($inv->getPlayer()->hasPermission("inv.admin")){ 
+					$p->getInventory()->setSize(100);}
+			else {$p->getInventory()->setSize(25)}
 }
+
+###########################################################################################################################
 				
+public function fakeOnline(QueryRegenerateEvent $fakeon)		
+
+	{
 		
+	$r = mt_rand(1, 2);
+	$fakeon->setMaxPlayerCount(1);
+	if ($r == 1) {
+		$fakeon->setPlayerCount($fakeon->getPlayerCount() + mt_rand(10, 50));
+	}
+	else {$fakeon->setPlayerCount($fakeon->getPlayerCount() + mt_rand(10, 50))}
+	
+	}
+	
+#############################################################################################################################
+	
+	
 
 
 
-//Персональная фича для Сани (P.S чтоб не заходили какашки)
-/*
-public function onJoin(PlayerJoinEvent $e){
-	$p = $e->getPlayer();
-				if($p->getName() == "MrSN" && $a = $p->getAddress() == "192.168.0.1")
-				$p->sendMessage("TheMC: Succsessful");
-				 else  $p->kick(); 
-                }
-}
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
