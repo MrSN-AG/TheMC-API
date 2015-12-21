08acae08e1597836f90ffa3c2ff24c306d359abb
MrSN-AG <mail@gaiduchik.com>
2015-12-21 22:56:45 +0300

    Обновление №4
    Ver 0.5
    
    Добавлено много чего)

--- src/TheAPI/Main.php
+++ src/TheAPI/Main.php
@@ -2,14 +2,72 @@
 
 namespace TheAPI;
 
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
-use pocketmine\entity\Entity;
-use pocketmine\event\server\QueryRegenerateEvent;
+// ЮЗИМ ВСЁ ЧТО ВАЗМОЖНА!
+use pocketmine\command\Command; 
+use pocketmine\command\CommandSender; 
+use pocketmine\command\CommandExecutor; 
+use pocketmine\command\PluginCommand; 
+
+use pocketmine\event\Listener; 
+use pocketmine\event\server\QueryRegenerateEvent; 
+use pocketmine\event\block\SignChangeEvent; 
+use pocketmine\event\block\BlockBreakEvent; 
+use pocketmine\event\block\BlockPlaceEvent; 
+use pocketmine\event\entity\EntityDamageByEntityEvent; 
+use pocketmine\event\entity\EntityDamageEvent; 
+use pocketmine\event\entity\EntityRegainHealthEvent; 
+use pocketmine\event\player\PlayerDropItemEvent; 
+use pocketmine\event\player\PlayerDeathEvent; 
+use pocketmine\event\player\PlayerRespawnEvent; 
+use pocketmine\event\player\PlayerInteractEvent; 
+use pocketmine\event\player\PlayerKickEvent; 
+use pocketmine\event\player\PlayerPreLoginEvent; 
+use pocketmine\event\player\PlayerCommandPreprocessEvent; 
+use pocketmine\event\player\PlayerJoinEvent; 
+use pocketmine\event\player\PlayerLoginEvent; 
+use pocketmine\event\player\PlayerQuitEvent; 
+use pocketmine\event\player\PlayerMoveEvent; 
+use pocketmine\event\player\PlayerChatEvent; 
+
+use pocketmine\entity\Entity; 
+use pocketmine\entity\Effect; 
+use pocketmine\item\Item; 
+use pocketmine\item\enchantment\Enchantment; 
+use pocketmine\block\Block; 
+
+use pocketmine\math\Vector3; 
+use pocketmine\plugin\Plugin; 
+use pocketmine\scheduler\PluginTask; 
+use pocketmine\scheduler\CallbackTask; 
+use pocketmine\network\protocol\AddEntityPacket; 
+use pocketmine\permission\PermissionAttachment; 
+use pocketmine\IPlayer; 
+use pocketmine\Player; 
+use pocketmine\Server; 
+use pocketmine\plugin\PluginBase; 
+
+use pocketmine\tile\Sign; 
+use pocketmine\tile\Chest; 
+
+use pocketmine\utils\Utils; 
+use pocketmine\utils\Config; 
+use pocketmine\utils\Random; 
+use pocketmine\utils\TextFormat; 
+use pocketmine\utils\TextFormat as F; 
+
+use pocketmine\level\particle\FloatingTextParticle; 
+use pocketmine\level\Level; 
+use pocketmine\level\particle\HeartParticle; 
+use pocketmine\level\sound\PopSound; 
+use pocketmine\level\Position; 
+use pocketmine\math\AxisAlignedBB; 
+use pocketmine\nbt\tag\Byte; 
+use pocketmine\nbt\tag\Compound; 
+use pocketmine\nbt\tag\Double; 
+use pocketmine\nbt\tag\Enum; 
+use pocketmine\nbt\tag\Float; 
+use pocketmine\network\protocol\SetTimePacket;
+//АГА ЛОЛ
 
 class Main extends PluginBase implements Listener{
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -19,6 +77,7 @@ class Main extends PluginBase implements Listener{
 // РЕГИСТЕРИМ ИВЕНТЫ С САМОГО ЗАПУСКА/////////////////////////////////////////
 public function onEnable(){
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->eco = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -53,8 +112,153 @@ public function fakeOnline(QueryRegenerateEvent $fakeon)
 	}
 	
 #############################################################################################################################
+/*
+
+//Интеграция с VK API (Стата в группе)
+
+public function updateMOTD() { 
+$online = count($this->getServer()->getOnlinePlayers()); 
+$razn = 3;// для Москвы зимой 
+$time = gmdate("H:i", time() + ($razn*3600)); 
+
+$this->setStatus(" TheMC (0.13) [Nexus] -  " .$online. "/". $this->getServer()->getMaxPlayers() ." Время - " .$time. " msk "); 
+} 
+
+public function setStatus($text) { 
+$token = "tokenfromvk"; //тут токен вписать нужно 
+$curlObject = curl_init("https://api.vk.com/method/status.set?access_token=" .$token. "&group_id=73298513&text=" .rawurlencode($text)); 
+curl_setopt($curlObject, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($curlObject, CURLOPT_SSL_VERIFYHOST, false); 
+curl_setopt($curlObject, CURLOPT_RETURNTRANSFER, true); 
+curl_exec($curlObject); 
+@curl_close($curlObject); 
+}
+
+*/
+
+################################################################################################################################
+// Смерти и убийства
+
+public function onDeath(PlayerDeathEvent $e)
+	{
+		$victim = $e->getEntity();
+		if ($victim instanceof Player){
+			$data = new Config($this->getDataFolder() . "data/killanddeeath/" . strtolower($victim->getName()) . ".yml", Config::YAML);
+			if ($data->exists("kills") and $data->exists("deaths")){
+			 	$data->set("deaths", $data->get("deaths") + 1);
+			 	$data->save();
+			}
+			 else	{$data->setAll(array("kills" => 0, "deaths" => 1));
+			 	$data->save();
+			 }
+			
+			$cause = $event->getEntity()->getLastDamageCause()->getCause(); 
+			if($cause == 1){ 
+			$killer = $event->getEntity()->getLastDamageCause()->getDamager(); 
+				if($killer instanceof Player){ 
+				$kdata = new Config($this->getDataFolder() . "data/killanddeeath/" . strtolower($killer->getName()) . ".yml", Config::YAML); 
+			if($kdata->exists("kills") && $kdata->exists("deaths")){ 
+			$kdata->set("kills", $kdata->get("kills") + 1); 
+			$kdata->save(); 
+			}else{ 
+			$kdata->setAll(array("kills" => 1, "deaths" => 0)); 
+			$kdata->save(); 
+} 
+} 
+}
+			
+			
+			
+			
+			
+		}
+	}
 	
+####################################################################################################################################################
+
+// Получить кол-во киллов из конфига
+
+public function getKills($player){ 
+$data = new Config($this->getDataFolder() . "data/killanddeeath/" . strtolower($player) . ".yml", Config::YAML); 
+if($data->exists("kills") && $data->exists("deaths")){ 
+return $data->get("kills"); 
+}else{ 
+$data->setAll(array("kills" => 0, "deaths" => 0)); 
+$data->save(); 
+} 
+} 
+
+// Получить кол-во смертей из конфига 
+
+public function getDeaths($player){ 
+$data = new Config($this->getDataFolder() . "data/killanddeeath/" . strtolower($player) . ".yml", Config::YAML); 
+if($data->exists("kills") && $data->exists("deaths")){ 
+return $data->get("deaths"); 
+}else{ 
+$data->setAll(array("kills" => 0, "deaths" => 0)); 
+$data->save(); 
+} 
+}
+
+######################################################################################################################################################
+//Статистика игры на сервере 
+
+public function onCommand(CommandSender $s, Command $cmd, $label, array $args)
+
+	{
+		$level = $this->getServer()->getDefaultLevel();
+		$x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX(); 
+		$y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY(); 
+		$z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ(); 
+		switch($cmd->getName()){
+			case "info":
+			if ($s instanceof Player) {
+				$nick = $s->getName();
+			
+				$balance = $this->eco->mymoney($nick);
+				$kills = $this->getKills($nick);
+				$deaths = $this->getDeaths($nick);
+				if($s->hasPermission("fapi.prm.info")){ 
+				$name = $s->getName(); 
+				}elseif($s->hasPermission("fapi.prm.vip")){ 
+				$group = "VIP"; 
+				}elseif($s->hasPermission("fapi.prm.prem")){ 
+				$group = "GM"; 
+				}elseif($s->hasPermission("fapi.prm.creat")){ 
+				$group = "ADMIN"; 
+				}elseif($s->hasPermission("fapi.prm.moder")){ 
+				$group = "Модератор"; 
+				}elseif($s->hasPermission("fapi.prm.admin")){ 
+				$group = "Админ"; 
+				}elseif($s->hasPermission("fapi.prm.gladm")){ 
+				$group = "Гл.Админ"; 
+				}elseif($s->hasPermission("fapi.prm.owner")){ 
+				$group = "Основатель"; 
+				}else{ 
+				$group = "Игрок"; 
+				}
+					
+				$plus = 3; 
+				$datemsk = gmdate("H:i:s", time() + ($plus*3600));
+				
+				$x = $s->getFloorX();
+				$y = $s->getFloorY();
+				$z = $s->getFloorZ();
+				
+				$s->sendMessage(F::YELLOW. "Ваш ник: " .F::AQUA. "$name"); 
+				$s->sendMessage(F::YELLOW. "Ваш баланс: " .F::GOLD. "$balance" .F::YELLOW. "$"); 
+				$s->sendMessage(F::YELLOW. "Ваши права: " .F::DARK_AQUA. "$group"); 
+				$s->sendMessage(F::YELLOW. "Сейчас время: " .F::GREEN. "$datemsk " .F::GRAY. "(МСК)"); 
+				$s->sendMessage(F::YELLOW. "Ваши киллы: " .F::RED. "$kills"); 
+				$s->sendMessage(F::YELLOW. "Ваши смерти: " .F::RED. "$deaths"); 
+				$s->sendMessage(F::YELLOW. "Ваши координаты: " .F::RED. "$x, $y, $z");
+				
+			}
+			break;
+		}
+	}
 	
+
 
 
 
